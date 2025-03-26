3cef685ae1792cb1463bfd91082a937e3eb1108d
Jose Herrera <jaherreraf4@gmail.com>
2025-03-26 17:53:40 -0400

    change in login and register

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -72,7 +72,7 @@
 <template>
   <div class="w-screen h-dvh bg-violet-100 grid place-content-start md:place-content-center">
     <!-- Contenedor principal con dos columnas -->
-    <div id="container" class="bg-violet-500 md:w-[700px] h-dvh md:h-[500px] grid md:grid-cols-2 md:rounded-2xl overflow-hidden">
+    <div id="container" class="bg-violet-500 md:w-[700px] h-dvh md:h-[600px] grid md:grid-cols-2 md:rounded-2xl overflow-hidden">
       <!-- Columna 1: Formulario de inicio de sesión -->
       <div class="bg-white rounded-l-2xl  md:h-full p-12 flex flex-col items-center justify-center gap-8 row-start-2 md:row-start-1">
         <h1 class="font-black text-4xl text-violet-900">Iniciar Sesión</h1>
@@ -80,13 +80,13 @@
           <!-- Campo de identificación -->
           <div class="relative">
             <input
-              class="w-full h-20 bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border-2 border-violet-900 rounded-md px-3 py-2 pt-4 transition duration-300 ease focus:outline-none focus:border-violet-950 hover:border-slate-300 shadow-sm focus:shadow"
+              class="w-full h-18 bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border-2 border-violet-900 rounded-md px-3 py-2 pt-6 transition duration-300 ease focus:outline-none focus:border-violet-950 hover:border-slate-300 shadow-sm focus:shadow"
               name="id"
               id="id"
               v-model="identification"
             />
             <label
-              class="absolute cursor-text bg-white px-1 left-2.5 top-2.5 text-violet-900 text-lg transition-all transform origin-left"
+              class="absolute cursor-text bg-white px-1 left-2.5 top-2.5 text-violet-900 text-md transition-all transform origin-left"
               for="id"
             >
               Usuario o identificación
@@ -95,14 +95,14 @@
           <!-- Campo de contraseña -->
           <div class="relative">
             <input
-              class="w-full h-20 bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border-2 border-violet-900 rounded-md px-3 py-2 pt-4 transition duration-300 ease focus:outline-none focus:border-violet-950 hover:border-slate-300 shadow-sm focus:shadow"
+              class="w-full h-18 bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border-2 border-violet-900 rounded-md px-3 py-2 pt-6 transition duration-300 ease focus:outline-none focus:border-violet-950 hover:border-slate-300 shadow-sm focus:shadow"
               type="password"
               name="password"
               id="password"
               v-model="password"
             />
             <label
-              class="absolute cursor-text bg-white px-1 left-2.5 top-2.5 text-violet-900 text-lg transition-all transform origin-left"
+              class="absolute cursor-text bg-white px-1 left-2.5 top-2.5 text-violet-900 text-md transition-all transform origin-left"
               for="password"
             >
               Contraseña
@@ -117,11 +117,11 @@
       </div>
 
       <!-- Columna 2: Mensaje de bienvenida -->
-      <div class="text-center p-12 flex flex-col items-center justify-center gap-4 text-white row-start-1">
-        <h2 class="text-4xl font-bold">Welcome to Login</h2>
-        <p class="text-lg">Ingrese sus datos para disfrutar de nuestra app.</p>
-        <span class="text-sm">¿Todavía no tienes cuenta? <a href="/register" class="text-violet-200 hover:underline">Haz clic aquí y regístrate.</a></span>
-        <small class="text-xs">Puedes continuar usando la app sin iniciar sesión. No contarás con todos los servicios.</small>
+      <div class="text-center p-12 flex flex-col items-center justify-center gap-4 text-white row-start-1 text-md">
+        <h2 class="text-4xl font-bold">Bienvenido a inicio de sección</h2>
+        <p>Ingrese sus datos para disfrutar de nuestra app.</p>
+        <span>¿Todavía no tienes cuenta? <a href="/register" class="text-violet-200 hover:underline">Haz clic aquí y regístrate.</a></span>
+        <small>Puedes continuar usando la app sin iniciar sesión. No contarás con todos los servicios.</small>
         <button class="mt-4 bg-white text-violet-900 py-2 px-6 rounded-lg hover:bg-violet-50 transition-colors cursor-pointer">
           Ir a la app
         </button>
